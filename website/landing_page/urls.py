8aee64a2d4c648e68c7d12d570220fc98f9b2801
tomasdanjo <danthorium25@gmail.com>
2024-10-02 22:31:04 +0800

    added redirect to full article feature

--- website/landing_page/urls.py
+++ website/landing_page/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import landing_page
+from .views import landing_page, full_article
 
 urlpatterns = [
-    # path('signup/',signup,name='signup'),
-    # path('login/',login,name='login')
     path('',landing_page,name='home'),
+    path('article/<int:id>/',full_article,name='full_article_view')
+    # path('article_detail/',landing_page,name="article_view")
 ]
--- website/landing_page/views.py
+++ website/landing_page/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 def landing_page(request):
     articles = Article.objects.all()
     return render(request, 'landing_page.html', {'articles': articles})
+
+def full_article(request, id):
+    article = get_object_or_404(Article,id=id)
+    return render(request,'full_article_view.html',{'article':article})
+
