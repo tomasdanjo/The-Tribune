db3926fb129f9d7df4b6f27418c7aae42d9bb455
tomasdanjo <danthorium25@gmail.com>
2024-10-02 18:32:07 +0800

    made bug fixes

--- website/website/urls.py
+++ website/website/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from user_authentication.views import signup, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/',signup,name='signup'),
-    path('login/',login,name='login')
+    path('',include('user_authentication.urls')),
+    path('',include('landing_page.urls')),
 ]
