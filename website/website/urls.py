630399095346dff64301145dd746a65bf7653d11
tomasdanjo <danthorium25@gmail.com>
2024-10-02 18:40:20 +0800

    made bug fixes to display photos

--- website/website/urls.py
+++ website/website/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from user_authentication.views import signup, login
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('user_authentication.urls')),
     path('',include('landing_page.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
