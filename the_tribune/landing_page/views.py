b3e8e939c80e3fff812f4edf8f8103b02c728e16
tomasdanjo <danthorium25@gmail.com>
2024-10-15 20:15:48 +0800

    fixed naive datetime

--- the_tribune/landing_page/views.py
+++ the_tribune/landing_page/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render,get_object_or_404
 from article.models import Article
 from .models import Comment
+from django.utils import timezone
+# from myapp.models import Article
 
 def landing_page(request):
     articles = Article.objects.all()
+    for article in articles:
+        if timezone.is_naive(article.date_published):
+            article.date_published = timezone.make_aware(article.date_published, timezone.get_current_timezone())
+            article.save()
+            
     return render(request, 'landing_page.html', {'articles': articles})
 
 def full_article(request, id):
     article = get_object_or_404(Article,id=id)
     comments = Comment.objects.filter(article_id=id)
     related_stories = Article.objects.filter(tag_id=article.tag_id).exclude(id=article.id)
+    
+
     return render(request,'full_article_view.html',{'article':article,'comments':comments,'related_stories':related_stories})
 
