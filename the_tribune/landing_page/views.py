e3e23fc07c86a526e707d038fd7f89be0e906684
Ryan <ryanjayanthonytimagos@gmail.com>
2024-10-25 00:03:12 +0800

    Popup Complete

--- the_tribune/landing_page/views.py
+++ the_tribune/landing_page/views.py
@@ -1,36 +1,36 @@
-from django.shortcuts import render,get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.utils import timezone
 from article.models import Article
 from .models import Comment, Subscription
-from django.contrib import messages 
-from django.utils import timezone
-# from myapp.models import Article
 
 def landing_page(request):
-    articles = Article.objects.filter(status    ='published')
+    articles = Article.objects.filter(status='published')
     for article in articles:
         if timezone.is_naive(article.date_published):
             article.date_published = timezone.make_aware(article.date_published, timezone.get_current_timezone())
             article.save()
-            
     return render(request, 'landing_page.html', {'articles': articles})
 
 def full_article(request, id):
-    article = get_object_or_404(Article,id=id)
+    article = get_object_or_404(Article, id=id)
     comments = Comment.objects.filter(article_id=id)
     related_stories = Article.objects.filter(tag_id=article.tag_id).exclude(id=article.id)
-    
+    return render(request, 'full_article_view.html', {
+        'article': article,
+        'comments': comments,
+        'related_stories': related_stories
+    })
 
-    return render(request,'full_article_view.html',{'article':article,'comments':comments,'related_stories':related_stories})
+
 
 def subscribe(request):
     if request.method == 'POST':
         email = request.POST.get('email')
         if email:
-            # Check if the email naa ni exist na ang email
             if Subscription.objects.filter(email=email).exists():
                 messages.info(request, 'You are already subscribed with this email.')
             else:
-                # Create new
                 Subscription.objects.create(email=email)
                 messages.success(request, 'Successfully subscribed to the newsletter!')
             return redirect('home')
@@ -38,5 +38,4 @@ def subscribe(request):
             messages.error(request, 'Please provide a valid email address.')
             return redirect('home')
     else:
-        # Optionally, handle GET requests or redirect
         return redirect('home')
