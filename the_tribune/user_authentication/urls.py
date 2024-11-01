d9461fa4f89c0ab32e2388d724b5c0d946e096c2
thomasdanjo <danthorium25@gmail.com>
2024-11-02 01:25:24 +0800

    added signup for readers

--- the_tribune/user_authentication/urls.py
+++ the_tribune/user_authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import login_view, signup_view
+from .views import login_view, signup_view, signup_user_view
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('signup/',signup_view,name='signup'),
     path('login/',login_view,name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('signupuser',signup_user_view,name='signup_user')
 ]
